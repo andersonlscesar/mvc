2eada284afed12a68d06cd11353c3a77566a10c4
Anderson Cesar <andersonlscesar@gmail.com>
2022-11-07 18:15:45 -0300

    menu dinamico

--- app/Controller/Admin/Page.php
+++ app/Controller/Admin/Page.php
@@ -5,6 +5,24 @@ use App\Utils\View;
 
 class Page 
 {
+
+    private static $modules = [
+        'Home'  => [
+            'label' => 'Home',
+            'link'  => URL.'/admin'
+        ],
+
+        'Depoimentos'  => [
+            'label' => 'Depoimentos',
+            'link'  => URL.'/depoimentos'
+        ],
+
+        'Usuarios'  => [
+            'label' => 'Usuarios',
+            'link'  => URL.'/usuarios'
+        ]
+    ];
+
     public static function renderMainLayout($title, $content)
     {
         return View::render('admin/page', [
@@ -17,9 +35,26 @@ class Page
     public static function renderPanel($title, $content, $currentModule)
     {
         $contentPanel = View::render('admin/panel', [
-            'menu'      => 'olá, mundo',
+            'menu'      => self::renderMenu($currentModule),
             'content'   => $content
         ]);
         return self::renderMainLayout($title, $contentPanel);
+    }
+
+
+    private static function renderMenu($currentModule)
+    {
+        $links = '';
+
+        foreach(self::$modules as $hash => $module) {
+            $links .= View::render('admin/menu/link', [
+                'label'     => $module['label'],
+                'link'      => $module['link'],
+                'current'   => $hash == $currentModule ? 'text-danger' : ''
+            ]);
+        }
+        return View::render('admin/menu/box', [
+            'links'  => $links
+        ]);
     }
 }
